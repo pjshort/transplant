79935938a1bcd453cb7ba862ce62ffd4283f2993
Patrick Short <pjshort42@gmail.com>
2015-06-16 15:00:18 +0100

    return significant gene module to trait associations

---
+++ scripts/correlation_tools.R
@@ -0,0 +1,24 @@
+# tools for assessing correlations between gene modules and phenotypes
+
+significant_corrs <- function(MEs, phenotypes, method = "pearson", fdr = 0.05){
+  
+  # takes module eigengene expression levels and phenotype data matrix
+  # returns dataframe of module to phenotype correlations with correlation strength and p value
+  
+  corr = WGCNA::cor(MEs, phenotypes, use = "p", method = method)
+  
+  # get p value from correlation and degrees of freedom
+  p = corPvalueStudent(corr, n_samples)
+  
+  # benjamini-hochberg error rate correction
+  p = apply(p, MARGIN = 2, FUN = function(p) p.adjust(p, length(corr), method = "fdr"))
+  
+  # get row and column with adjusted p < false discovery rate
+  l = which(p < fdr, arr.ind = TRUE)
+  modules = names(MEs)[l[,1]]
+  traits = colnames(phenotypes)[l[,2]]
+  
+  associations <- data.frame(cbind(modules, traits))
+  return(associations)
+  
+}
