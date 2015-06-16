5c39eda5aad708abaa15cc64026c541d7fbc6ffc
Patrick Short <pjshort42@gmail.com>
2015-06-16 16:38:59 +0100

    minor updates

--- scripts/correlation_tools.R
+++ scripts/correlation_tools.R
@@ -14,11 +14,14 @@ significant_corrs <- function(MEs, phenotypes, method = "pearson", fdr = 0.05){
   p_adjust = apply(p, MARGIN = 2, FUN = function(p) p.adjust(p, length(corr), method = "fdr"))
   
   # get row and column with adjusted p < false discovery rate
-  l = which(p_adjust < fdr, arr.ind = TRUE)
-  modules = names(MEs)[l[,1]]
-  traits = colnames(phenotypes)[l[,2]]
+  l_mat = which(p_adjust < fdr, arr.ind = TRUE)
+  l_flat = which(p_adjust < fdr)
+  modules = names(MEs)[l_mat[,1]]
+  traits = colnames(phenotypes)[l_mat[,2]]
+  cs = corr[l_flat]
+  ps = p[l_flat]
   
-  associations <- data.frame(cbind(modules, traits, corr, p))
+  associations <- data.frame(cbind(modules, traits, cs, ps))
   return(associations)
   
 }
